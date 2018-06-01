6d62bf40d6dff62b96f39b1114bb9f92b07f6d82
Matan Arbiv <matan.arbiv@emc.com>
2018-06-01 12:33:33 +0300

    Adding all controlers and views

---
+++ WebAppProj/Controllers/TitlesController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MasterProj.Models;
+using MasterProj.Models.DAL;
+
+namespace WebAppProj.Controllers
+{
+    public class TitlesController : Controller
+    {
+        private DataContext db = new DataContext();
+
+        // GET: Titles
+        public ActionResult Index()
+        {
+            var titles = db.Titles.Include(t => t.Author).Include(t => t.Genre);
+            return View(titles.ToList());
+        }
+
+        // GET: Titles/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Title title = db.Titles.Find(id);
+            if (title == null)
+            {
+                return HttpNotFound();
+            }
+            return View(title);
+        }
+
+        // GET: Titles/Create
+        public ActionResult Create()
+        {
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "FullName");
+            ViewBag.GenreID = new SelectList(db.Generes, "ID", "GenreType");
+            return View();
+        }
+
+        // POST: Titles/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,TitleName,Price,PublishedDate,AuthorID,GenreID")] Title title)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Titles.Add(title);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "FullName", title.AuthorID);
+            ViewBag.GenreID = new SelectList(db.Generes, "ID", "GenreType", title.GenreID);
+            return View(title);
+        }
+
+        // GET: Titles/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Title title = db.Titles.Find(id);
+            if (title == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "FullName", title.AuthorID);
+            ViewBag.GenreID = new SelectList(db.Generes, "ID", "GenreType", title.GenreID);
+            return View(title);
+        }
+
+        // POST: Titles/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,TitleName,Price,PublishedDate,AuthorID,GenreID")] Title title)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(title).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.AuthorID = new SelectList(db.Authors, "ID", "FullName", title.AuthorID);
+            ViewBag.GenreID = new SelectList(db.Generes, "ID", "GenreType", title.GenreID);
+            return View(title);
+        }
+
+        // GET: Titles/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Title title = db.Titles.Find(id);
+            if (title == null)
+            {
+                return HttpNotFound();
+            }
+            return View(title);
+        }
+
+        // POST: Titles/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Title title = db.Titles.Find(id);
+            db.Titles.Remove(title);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
